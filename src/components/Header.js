88c69df5adf8a387f6c095fa9768a5dd6381e1dd
Hans Ellis <bullhorn@gmail.com>
2023-03-27 20:30:43 -0700

    set up environment for react with redux
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <h1>Hang Person</h1>
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore();
+
+store.subscribe(() =>
+  console.log(store.getState())
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
